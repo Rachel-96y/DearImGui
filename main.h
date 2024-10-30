392bab67eee3721005399cf7d3ed521fef3c480b
Administrator <Administrator@DESKTOP-PL8E08J>
2024-10-30 18:43:09 +0800

    添加项目文件。

---
+++ main.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef __MAIN__
+#define __MAIN__
+
+#include <Windows.h>
+
+// ImGui
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+#include <d3d11.h>
+#include <tchar.h>
+
+// Detours
+#include "detours.h"
+#include "detver.h"
+
+// Font
+#include "Font/font.h"
+
+#endif // !__MAIN__
